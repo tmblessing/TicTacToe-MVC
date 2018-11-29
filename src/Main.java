22b08cae54737ef73391c18d13180beabb919336
Tom Blessing <tblessing@umass.edu>
2018-11-29 13:02:59 -0500

    Updated main comments

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main{
 	public static void main(String[] args) {
 		
-		//Instantiate components, and give them references to each other
+		//Instantiate components, and connect them
 		View viz = new View();
-        Model game = new Model(viz);
-		Controller cont = new Controller(game);
-		viz.setCont(cont);
+        	Model game = new Model(viz); 		//Model updates the view
+		Controller cont = new Controller(game); //Controller updates the model
+		viz.setCont(cont);			//And the view updates the controller
 		
 		//Start the game
 		game.generateBoard();
